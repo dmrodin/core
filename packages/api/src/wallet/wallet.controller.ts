551e72303199c1a864947d261067c8d315db514c
Denis Pominov <necrofallen@gmail.com>
2026-03-05 23:27:56 +0400

    put wallets fix for mod

--- packages/api/src/wallet/wallet.controller.ts
+++ packages/api/src/wallet/wallet.controller.ts
@@ -239,7 +239,7 @@ export class WalletController {
 
     @Put(':id')
     @HttpCode(HttpStatus.OK)
-    @Roles(RoleCode.admin)
+    @Roles(RoleCode.admin, RoleCode.moderator)
     @ApiOperation({
         summary: 'Обновить кошелек',
         description: 'Обновляет данные существующего кошелька. Доступно только администраторам.',
